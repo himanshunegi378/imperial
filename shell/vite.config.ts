96a7e142755562b0b8761dc0ca4306c3747a6baf
Himanshu <himanshunegi378@gmail.com>
2025-07-20 21:08:31 +0530

    added comporess

--- shell/vite.config.ts
+++ shell/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { compression, defineAlgorithm } from 'vite-plugin-compression2'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),
+  plugins: [react(), tailwindcss(),
+  compression({
+    algorithms: [
+      defineAlgorithm('gz', {
+        level: 9
+      })
+    ]
+
+  })
   ],
 })
