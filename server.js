93d3575e1c3a3122326470894fb7bfeebe1014b3
Himanshu <himanshunegi378@gmail.com>
2025-07-20 21:35:36 +0530

    origins added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import { env } from './env.js';
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://imperial.timercards.com', 'https://www.imperial.timercards.com'],
     credentials: true
 }));
 app.use(express.json());
