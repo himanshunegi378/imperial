e4b9521ed9321fbdba275c84ba699236dbff4ea2
Himanshu <himanshunegi378@gmail.com>
2025-07-26 21:32:09 +0530

    schema path cahgne

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("drizzle-kit");
 
 module.exports = defineConfig({
   dialect: 'sqlite', // 'mysql' | 'sqlite' | 'turso'
-  schema: './schema.js',
+  schema: './src/schema.ts',
 })
